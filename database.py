2a29ca3c702b2c1499284fda9b5763eb87fa99e5
kinserwe <ghfhdjjjdjdjj@gmail.com>
2026-08-05 18:25:07 +0300

    feat: add transfer and scan_state models with migrations

---
+++ database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+from config import settings
+
+engine = create_engine(settings.database_url, echo=settings.sql_echo)
+
+SessionFactory = sessionmaker(bind=engine)
+
+
+class Base(DeclarativeBase): ...
